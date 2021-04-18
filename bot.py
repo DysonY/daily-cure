90d759a5ddc8da25cce4e82f33d99b89d129270c
DysonY <dysonye1632@gmail.com>
2021-04-18 12:14:09 -0400

    catchphrases

--- bot.py
+++ bot.py
@@ -18,26 +18,33 @@ colors = {
     'red' : discord.Color.from_rgb(255, 0, 0),
     'purple' : discord.Color.from_rgb(165, 57, 254),
     'green'  : discord.Color.from_rgb(0, 255, 0),
-    'white' : discord.Color.from_rgb(255, 255, 255)
+    'white' : discord.Color.from_rgb(254, 254, 254)
 }
 
 @bot.command(name='cure', help='Display info on a random cure')
 async def cure(ctx):
     with open('cure_names.txt') as f1, open('civilian_names.txt') as f2:
+        # Read from file
         cure_names = f1.read().splitlines()
-
         civilian_names = f2.read().splitlines()
-
         index = random.randint(0, 69)
-        cure = cure_names[index].split(',')[0]
-        color = colors[cure_names[index].split(',')[1]]
+        data = cure_names[index].split(';')
+
+        # Get text to display
+        cure = data[0]
+        color = colors[data[1]]
+        phrase = data[2]
         civilian = civilian_names[index]
+
+        # TODO: images, quotes
+        # formatting tutoral: https://python.plainenglish.io/python-discord-bots-formatting-text-efca0c5dc64a
 
         url_ext = civilian.replace(' ', '_')
 
         response = discord.Embed(
             title = cure + '/' + civilian,
             url = 'https://prettycure.fandom.com/wiki/' + url_ext,
+            description = phrase,
             color = color
         )
         await ctx.send(embed=response)
